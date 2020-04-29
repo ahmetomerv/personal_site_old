6bb58bba34999b0ad23c951fe3673272ff3871ed
Ahmet Ömer <ahmetomerv@gmail.com>
2020-04-29 09:59:30 +0300

    added new project, indentation stuff

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,39 +3,44 @@ import { IProject } from 'src/app/interfaces/project.interface';
 import { GithubService } from 'src/app/services/github.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  personalProjects: IProject[];
+	personalProjects: IProject[];
 	workProjects: IProject[] = [
 		{
-			name: "İndeks - Aksigorta",
-			description: "Documents indexing application",
+			name: 'İndeks - Aksigorta',
+			description: 'Documents indexing application',
 			path: '/project/indeks'
 		},
 		{
-			name: "SAT - Aksigorta",
-			description: "Insurance management application",
+			name: 'SAT - Aksigorta',
+			description: 'Insurance management application',
 			path: '/project/sat'
 		},
 		{
-			name: "Dijital Depo - Türk Telekom",
-			description: "Online file storage service",
+			name: 'Dijital Depo - Türk Telekom',
+			description: 'Online file storage service',
 			path: '/project/dijitaldepo'
 		},
 		{
-			name: "Marketplace - Türk Telekom",
-			description: "Software & services market",
+			name: 'Marketplace - Türk Telekom',
+			description: 'Software & services market',
 			path: '/project/marketplace'
+		},
+		{
+			name: 'Barclaycard Web - Barclaycard',
+			description: 'Internet & mobile banking',
+			path: '/project/barclaycard'
 		}
-	]
+	];
 
-  constructor(private githubService: GithubService) {}
+	constructor(private githubService: GithubService) {}
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 		this.githubService.getUserRepositories().subscribe(res => this.personalProjects = res);
 	}
 
